2515a7bcfee4eaa9d26998612e9c1c054579a4f2
phtf92 <phtf92@gmail.com>
2020-06-02 20:31:12 -0300

    Updates

---
+++ precache-manifest.b051174f069fc12f8fa6983deedd878d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770038530806ab175bcd1adbf9124e43",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "9b2ed22573eb778d9cf3",
+    "url": "/recipe-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0542747e5e90aa6b868e",
+    "url": "/recipe-app/static/js/2.5910af42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.5910af42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2ed22573eb778d9cf3",
+    "url": "/recipe-app/static/js/main.f2103750.chunk.js"
+  },
+  {
+    "revision": "7ff5b27d3f9efd2f4352",
+    "url": "/recipe-app/static/js/runtime-main.96283c75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
